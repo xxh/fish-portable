a1ad1b5d47903cd23e1c5d4f604e55a9ee0e1f0b
stellarpower <stellarpower@googlemail.com>
2023-09-06 19:31:04 +0000

    Use realpath in fish.sh
    This allows symlinks to the shell script - the directory in which it resides was evaluating to the location of the link, not its target. Not exhaustively tested, feel free to change.

--- fish.sh
+++ fish.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 export TERMINFO_DIRS=/lib/terminfo:/etc/terminfo:/usr/share/terminfo:$TERMINFO_DIRS
-CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+CURRENT_DIR="$(cd "$(dirname "$(realpath "$0" )" )" && pwd)"
 $CURRENT_DIR/fish "$@"
